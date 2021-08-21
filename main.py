26e3b614cf7d9bde17fd17d0b927b8b54412a9f4
Katie <66881113+KatieFauci@users.noreply.github.com>
2021-08-21 07:06:54 +0000

    file path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 
 client = discord.Client()
 
@@ -12,21 +13,23 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('<3*hello'):
         await message.channel.send('Hello!')
 
-    if message.content.startswith('$pour-some-suga-on-me'):
+    if message.content.startswith('<3*pour-some-suga-on-me'):
       await message.channel.send('Show a sexy picture of suga')
 
-    if message.content.startswith('$lets-get-handsy'):
+    if message.content.startswith('<3*lets-get-handsy'):
       await message.channel.send('Show a picture of BTS hands')
 
-    if message.content.startswith("$namtiddies"):
+    if message.content.startswith("<3*namtiddies"):
       await message.channel.send('show a picture of RMs chest')
 
-
-
-    if message.content.startswith('$get-image'):
-        await message.channel.send(file=discord.File('army-logo.jpg'))
+    if message.content.startswith('<3*get-image'):
+      image_num = random.randint(0,199)
+      filepath = f'images/suga/{image_num}_suga.jpg'
+      #await message.channel.send(file=discord.File('1_suga.jpg'))
+      await message.channel.send(file=discord.File(filepath))
+      #https://replit.com/@KatieFauci/BTSArmyBot#images/suga/1_suga.jpg
 
 client.run(os.getenv('TOKEN'))
